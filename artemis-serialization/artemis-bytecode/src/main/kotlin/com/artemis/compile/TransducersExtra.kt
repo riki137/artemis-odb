dbefeefd397dd122abde53f426019c0ccda34216
Adrian Papari <a@onedaybeard.net>
2017-01-28 19:28:54 +0100

    subduce

--- artemis-serialization/artemis-bytecode/src/main/kotlin/com/artemis/compile/TransducersExtra.kt
+++ artemis-serialization/artemis-bytecode/src/main/kotlin/com/artemis/compile/TransducersExtra.kt
@@ -10,14 +10,14 @@ import java.util.concurrent.atomic.AtomicBoolean
 fun <R : MutableList<A>, A, B> intoList(xf: Transducer<A, B>,
                                         init: R = mutableListOf<A>() as R,
                                         input: Iterable<B>): R {
-	return transduce(xf, object : ReducingFunction<R, A> {
-		override fun apply(result: R,
-		                   input: A,
-		                   reduced: AtomicBoolean): R {
-			result.add(input)
-			return result
-		}
-	}, init, input)
+    return transduce(xf, object : ReducingFunction<R, A> {
+        override fun apply(result: R,
+                           input: A,
+                           reduced: AtomicBoolean): R {
+            result.add(input)
+            return result
+        }
+    }, init, input)
 }
 
 
@@ -27,123 +27,112 @@ class Key<K> // <-- hah! type infer it below
 @Suppress("UNCHECKED_CAST")
 fun <R, A, B, K> intoMap(xf: Transducer<A, B>,
                          init: R = mutableMapOf<K, A>() as R,
-                         keyType: Key<K> = Key<K>(),
+                         keyType: Key<K> = Key<Any>() as Key<K>,
                          input: Iterable<B>): R
-	where R : Map<K, A> {
+        where R : Map<K, A> {
 
-	val rf = object : ReducingFunction<R, A> {
-		override fun apply(result: R,
-		                   input: A,
-		                   reduced: AtomicBoolean): R = result
-	}
+    val rf = object : ReducingFunction<R, A> {
+        override fun apply(result: R,
+                           input: A,
+                           reduced: AtomicBoolean): R = result
+    }
 
-	return transduce(xf, rf, init, input)
+    return transduce(xf, rf, init, input)
 }
 
 inline fun <reified A : Any, B> subduce(xf: Transducer<A, B>): ReducingFunction<A, B> {
-	val rf: ReducingFunction<A, A> = object : ReducingFunction<A, A> {
-		override fun apply(): A {
-			return when (A::class) {
-				Boolean::class -> false as A
-				Byte::class    -> 0 as A
-				Short::class   -> 0 as A
-				Int::class     -> 0 as A
-				Long::class    -> 0 as A
-				Float::class   -> 0 as A
-				Double::class  -> 0 as A
-				else           -> A::class.java.newInstance()
-			}
-		}
+    val rf: ReducingFunction<A, A> = object : ReducingFunction<A, A> {
+        override fun apply(): A {
+            return when (A::class) {
+                Boolean::class   -> false as A
+                Byte::class      -> 0 as A
+                Character::class -> ' ' as A
+                Short::class     -> 0 as A
+                Int::class       -> 0 as A
+                Long::class      -> 0 as A
+                Float::class     -> 0 as A
+                Double::class    -> 0 as A
+                else             -> A::class.java.newInstance()
+            }
+        }
 
-		override fun apply(result: A,
-		                   input: A,
-		                   reduced: AtomicBoolean): A = input
-	}
+        override fun apply(result: A,
+                           input: A,
+                           reduced: AtomicBoolean): A = input
+    }
 
-	return xf.apply(rf)
+    return xf.apply(rf)
 }
 
 
-inline fun <A, reified K : Any, reified V : Any> makePair(left: Transducer<K, A>,
-                                              right: Transducer<V, A>) = object : Transducer<Pair<K, V>, A> {
+inline fun <A, reified K, reified V> makePair(left: Transducer<K, A>,
+                                              right: Transducer<V, A>): Transducer<Pair<K, V>, A>
+        where K : Any, V : Any {
 
-	val leftRf: ReducingFunction<K, A> = subduce(left)
-	val rightRf: ReducingFunction<V, A> = subduce(right)
+    return object : Transducer<Pair<K, V>, A> {
+        val leftRf: ReducingFunction<K, A> = subduce(left)
+        val rightRf: ReducingFunction<V, A> = subduce(right)
 
-	override fun <R> apply(rf: ReducingFunction<R, Pair<K, V>>) = object : ReducingFunction<R, A> {
-		override fun apply(result: R,
-		                   input: A,
-		                   reduced: AtomicBoolean): R {
+        override fun <R> apply(rf: ReducingFunction<R, Pair<K, V>>): ReducingFunction<R, A> {
+            return object : ReducingFunction<R, A> {
+                override fun apply(result: R,
+                                   input: A,
+                                   reduced: AtomicBoolean): R {
 
-			val l = leftRf.apply(leftRf.apply(), input, reduced)
-			val r = rightRf.apply(rightRf.apply(), input, reduced)
+                    val l = leftRf.apply(leftRf.apply(), input, reduced)
+                    val r = rightRf.apply(rightRf.apply(), input, reduced)
 
-			return rf.apply(result, Pair(l, r), reduced)
-		}
-	}
+                    return rf.apply(result, Pair(l, r), reduced)
+                }
+            }
+        }
+    }
 }
+
 
 // @todo collect to (array/map)-of-(list/set/bitset)
-// FIXME isolate transducer+rf fork/leaves
-//fun <A, K, V> makePair(left: Transducer<K, A>,
-//                       right: Transducer<V, A>) = object : Transducer<Pair<K, V>, A> {
-//
-//	val leftRf : ReducingFunction<K, A> = object : ReducingFunction<K, A>  {
-//		override fun apply(result: K, input: A, reduced: AtomicBoolean): K {
-//			left.apply(leftRf)
-//		}
-//		override fun apply(result: Any,
-//		                   input: A,
-//		                   reduced: AtomicBoolean): K {
-//			left.apply()
-//		}
-//
-//	}
-//
-//	override fun <R> apply(rf: ReducingFunction<R, Pair<K, V>>) = object : ReducingFunction<R, A> {
-//		override fun apply(): R {
-//			TODO("hi")
-//		}
-//
-//		override fun apply(result: R): R {
-//			TODO("hi")
-//		}
-//
-//		override fun apply(result: R,
-//		                   input: A,
-//		                   reduced: AtomicBoolean): R {
-//
-////			val leftRf = left.apply(rf)
-//			val l = left.apply(result as K, input, reduced)
-//			val r = right.apply(result as V, input, reduced)
-//
-//			return rf.apply(result, Pair(l, r), reduced)
-//		}
-//	}
-//}
+inline fun <K, V> groupBy() : Transducer<Map<K, Iterable<V>>, Pair<K, V>> {
+    return object : Transducer<Map<K, Iterable<V>>, Pair<K, V>> {
+        override fun <R> apply(rf: ReducingFunction<R, Map<K, Iterable<V>>>): ReducingFunction<R, Pair<K, V>> {
+            return object : ReducingFunction<R, Pair<K, V>> {
+                override fun apply(result: R,
+                                   input: Pair<K, V>,
+                                   reduced: AtomicBoolean): R {
+
+                    val key = input.first
+                    val group = result as MutableMap<K, MutableList<V>>
+                    val list = group[key] ?: mutableListOf<V>().apply { group[key] = this }
+                    list += input.second
+
+                    return result
+                }
+            }
+        }
+    }
+}
 
 fun <A, K> groupBy(f: (A) -> K) = object : Transducer<Map<K, Iterable<A>>, A> {
-	override fun <R> apply(rf: ReducingFunction<R, Map<K, Iterable<A>>>): ReducingFunction<R, A> {
-		return object : ReducingFunction<R, A> {
-			override fun apply(): R = rf.apply()
+    override fun <R> apply(rf: ReducingFunction<R, Map<K, Iterable<A>>>): ReducingFunction<R, A> {
+        return object : ReducingFunction<R, A> {
+            override fun apply(): R = rf.apply()
 
-			override fun apply(result: R,
-			                   input: A,
-			                   reduced: AtomicBoolean): R {
+            override fun apply(result: R,
+                               input: A,
+                               reduced: AtomicBoolean): R {
 
-				val key: K = f(input)
+                val key: K = f(input)
 
-				@Suppress("UNCHECKED_CAST")
-				val group = result as MutableMap<K, MutableList<A>>
-				val list = group[key] ?: mutableListOf<A>().apply { group[key] = this }
-				list += input
+                @Suppress("UNCHECKED_CAST")
+                val group = result as MutableMap<K, MutableList<A>>
+                val list = group[key] ?: mutableListOf<A>().apply { group[key] = this }
+                list += input
 
-				return result
-			}
-		}
-	}
+                return result
+            }
+        }
+    }
 }
 
 
 operator fun <A, B, C> Transducer<B, C>.plus(right: Transducer<A, in B>): Transducer<A, C>
-	= this.comp(right)
+        = this.comp(right)
--- artemis-serialization/artemis-bytecode/src/test/kotlin/com/artemis/compile/TransducerTest.kt
+++ artemis-serialization/artemis-bytecode/src/test/kotlin/com/artemis/compile/TransducerTest.kt
@@ -25,31 +25,27 @@ class TransducerTest {
 		) assertEquals mapOf()
 	}
 
-	@Test
-	fun test_subduce() {
-		val pairing = makePair(left = map { i: Int -> i * 3},
-		                       right = map { i: Int -> i * 2 })
+    @Test
+    fun test_subduce() {
+        intoList(xf = makePair(map { i: Int -> i % 2 },
+                               map { i: Int -> i * 2 }),
+                 input = (0..5)
+        ) assertEquals listOf(0 to 0,
+                              1 to 2,
+                              0 to 4,
+                              1 to 6,
+                              0 to 8,
+                              1 to 10)
 
-		intoList(xf = pairing,
-		         input = (0..9)).let(::println)
+        val pairing = makePair(left = map { i: Int -> i % 2 } + map(Int::toString),
+                               right = map { i: Int -> i * 2 })
+        intoMap(xf = pairing + groupBy<String, Int>(),
+                keyType = Key<Any>(), // <- heh, type-erasure
+                input = (0..9)
+        ) assertEquals mapOf("0" to listOf(0, 4, 8, 12, 16),
+                             "1" to listOf(2, 6, 10, 14, 18))
 
-//		intoList(xf = subduce(map(Int::toString)),
-//		          init = mutableListOf<String>(),
-//		          input = (0..9)
-//		) assertEquals mapOf(0 to listOf(0, 2, 4, 6, 8),
-//		                     1 to listOf(1, 3, 5, 7, 9))
-
-		intoMap(xf = groupBy { input: Int -> input and 0x1 },
-		        keyType = Key<Int>(),
-		        input = (0..9)
-		) assertEquals mapOf(0 to listOf(0, 2, 4, 6, 8),
-		                     1 to listOf(1, 3, 5, 7, 9))
-
-		intoMap(xf = groupBy { input: Int -> input and 0x1 },
-		        keyType = Key<Int>(),
-		        input = listOf<Int>()
-		) assertEquals mapOf()
-	}
+    }
 
 	@Test
 	fun test_make_pair() {
